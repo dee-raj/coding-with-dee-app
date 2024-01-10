fe1e02bcdfb643a38069426fc1ad461dd7d78338
dee-raj <dhurbaraj343@gmail.com>
2024-01-10 19:05:38 +0530

    Stack navigation and navigate to CourseDetailScreen

--- App/Components/HomeScreen/CourseList.js
+++ App/Components/HomeScreen/CourseList.js
@@ -1,11 +1,13 @@
-import { View, Text, FlatList, Image } from 'react-native'
+import { View, FlatList, TouchableOpacity } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { getCourseList } from '../../Services'
 import SubHeading from '../SubHeading';
 import Colors from '../../Utils/Colors';
-import { FontAwesome5 } from '@expo/vector-icons';
+import CourseItem from './CourseItem';
+import { useNavigation } from '@react-navigation/native';
 
 export default function CourseList({ level }) {
+   const navigation = useNavigation();
    const [courseList, setCouseList] = useState([]);
    useEffect(() => {
       getCourses();
@@ -27,64 +29,9 @@ export default function CourseList({ level }) {
             horizontal={true}
             showsHorizontalScrollIndicator={false}
             renderItem={({ item }) => (
-               <View style={{
-                  padding: 10,
-                  backgroundColor: Colors.bgColor,
-                  marginRight: 15,
-                  borderRadius: 15
-               }}>
-                  <Image source={{ uri: item?.banner?.url }}
-                     style={{
-                        width: 204, height: 205,
-                        borderRadius: 15
-                     }} />
-                  <View style={{ padding: 7 }}>
-                     <Text style={{
-                        fontFamily: 'sans-serif-medium',
-                        fontSize: 18,
-                     }}>{item?.name}</Text>
-                     <View style={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        justifyContent:'space-between',
-                     }}>
-                        <View style={{
-                           display: 'flex',
-                           flexDirection: 'row',
-                           alignItems: 'center',
-                           gap: 7,
-                           marginTop: 10,
-                        }}>
-                           <FontAwesome5 name="book-reader" size={27} color={Colors.dark_primary} />
-                           <Text style={{fontFamily:'serif'}}>{item?.chapters?.length} Chapters</Text>
-                        </View>
-                        <View style={{
-                           display: 'flex',
-                           flexDirection: 'row',
-                           alignItems: 'center',
-                           gap: 7,
-                           marginTop: 10,
-                        }}>
-                           <FontAwesome5 name="clock" size={19} color={Colors.dark_primary} />
-                           <Text style={{fontFamily:'sans-serif-thin'}}>{item?.time}</Text>
-                        </View>
-                     </View>
-                        <View style={{
-                           display: 'flex',
-                           flexDirection: 'row',
-                           alignItems: 'center',
-                           gap: 7,
-                           marginTop: 10,
-                        }}>
-                           <FontAwesome5 name="rupee-sign" size={18} color={Colors.golden} />
-                           <Text style={{
-                              color:Colors.golden,
-                              fontFamily:'notoserif',
-                              fontWeight:'bold'
-                           }}>{item.price==0? "Free": item?.price}</Text>
-                        </View>
-                  </View>
-               </View>
+               <TouchableOpacity onPress={()=>{navigation.navigate('course-detail')}}>
+                  <CourseItem item={item}/>
+               </TouchableOpacity>
             )}
          />
       </View>
